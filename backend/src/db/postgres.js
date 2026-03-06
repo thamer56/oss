446418a128d7621c82ac2593a4d593858e6e8fe6
thamer <thamerbelghalmi@gmail.com>
2026-03-06 01:18:53 +0100

    Fix production DB connection

--- backend/src/db/postgres.js
+++ backend/src/db/postgres.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL || 'postgresql://admin_oss:oss_admin_2024@ep-young-waterfall-a2z5zsqs.eu-central-1.aws.neon.tech/oss_db?sslmode=require',
     ssl: { rejectUnauthorized: false }
 });
 
