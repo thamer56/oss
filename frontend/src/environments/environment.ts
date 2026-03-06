4575508a3b4d6ed4cd0d1946ea6ae9d75feb7cf3
thamer <thamerbelghalmi@gmail.com>
2026-03-06 01:53:04 +0100

    Fix mixed content error by using prod URL in dev environment

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api'
+  apiUrl: 'https://oss-backend-r9wb.onrender.com/api' // Using Render URL for both local and prod to avoid Vercel mixed content
 };
